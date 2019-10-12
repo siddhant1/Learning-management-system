5ee4b4124825c6b7c0d89852ae4beb619bd338ea
Shivansh Tyagi <stshivansh9@gmail.com>
2019-10-13 01:39:32 +0530

    added the card layout on route /createPlaylist

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import { Route, Switch } from 'react-router-dom'
 import './App.css'
 import MainPage from './components/main-page/MainPage'
 import ScribblePad from './components/scribble-pad/ScribblePad'
+import CreatePlaylist from './components/createPlaylist/CreatePlaylist'
+import ReactPath from './lib/ReactPath'
+
 function App() {
-  return (
-    <div className='App'>
-      <Switch>
-        <Route path='/' exact component={MainPage} />
-        <Route path='/scribble' component={ScribblePad} />
-        {/* <Route path="/webcamrec" component={WebCamRecorder} />
-      <Route path="/audiorec" component={AudioRecorder} /> */}
-      </Switch>
-    </div>
-  )
+   return (
+      <div className='App'>
+         <Switch>
+            <Route path={ReactPath.homePath} exact component={MainPage}/>
+            <Route path={ReactPath.scribble} component={ScribblePad}/>
+            <Route path={ReactPath.createPlaylist} component={CreatePlaylist}/>
+            {/*<Route path="/webcamrec" component={WebCamRecorder} />*/}
+            {/*<Route path="/audiorec" component={AudioRecorder} />*/}
+         </Switch>
+      </div>
+   )
 }
 
 export default App
