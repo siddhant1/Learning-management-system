7b344c1480768591f08817270f3ea7cba4d943c5
shakti97 <shaktisingh230797@gmail.com>
2019-10-13 13:24:39 +0530

    Refactor Screen Recording Support Code

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import { Route, Switch } from 'react-router-dom'
 import './App.css'
 import MainPage from './components/main-page/MainPage'
 import ScribblePad from './components/scribble-pad/ScribblePad'
+// import RecordingAPI from './components/recordingApi/recordingApi2'
+import RecordingApi from './components/recordingApi/recordingApi';
+import WebCam from './components/webCam/webCam';
+
 function App() {
   return (
     <div className='App'>
       <Switch>
         <Route path='/' exact component={MainPage} />
         <Route path='/scribble' component={ScribblePad} />
+        <Route path='/recordingScreen' component={RecordingApi}/>
+        <Route path='/webCam' component={WebCam}/>
         {/* <Route path="/webcamrec" component={WebCamRecorder} />
       <Route path="/audiorec" component={AudioRecorder} /> */}
       </Switch>
--- src/components/recordingApi/recordingApi.js
+++ src/components/recordingApi/recordingApi.js
@@ -1,112 +1,104 @@
 import React from 'react'
-var preview, recording, downloadButton, logElement
-const recordingTimeMS = 10000;
 class RecordingApi extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-  componentDidMount() {
-    preview = document.getElementById('preview')
-    recording = document.getElementById('recording')
-    downloadButton = document.getElementById('downloadButton')
-    logElement = document.getElementById('log')
-  }
-  log = msg => {
-    //logElement.innerHTML += msg + "\n";
-  }
-
-  wait = delayInMS => {
-    return new Promise(resolve => setTimeout(resolve, delayInMS))
-  }
-
-  start = (stream, lengthInMS) => {
-    let recorder = new MediaRecorder(stream)
-    let data = []
-
-    recorder.ondataavailable = event => data.push(event.data)
-    recorder.start()
-    this.log(recorder.state + ' for ' + lengthInMS / 1000 + ' seconds...')
-
-    let stopped = new Promise((resolve, reject) => {
-      recorder.onstop = resolve
-      recorder.onerror = event => reject(event.name)
-    })
-
-    let recorded = this.wait(lengthInMS).then(
-      () => recorder.state === 'recording' && recorder.stop()
-    )
-
-    return Promise.all([stopped, recorded]).then(() => data)
-  }
-
-  stop = stream => {
-    stream.getTracks().forEach(track => track.stop())
-  }
-
-  startRecording = () => {
-    navigator.mediaDevices
-      .getDisplayMedia({
-        video: {
-            cursor: 'always'
-          },
-          audio: {
-            echoCancellation: true,
-            noiseSuppression: true,
-            sampleRate: 44100
-          }
-      })
-      .then(stream => {                   
-        preview.srcObject = stream
-        downloadButton.href = stream
-        preview.captureStream =
-          preview.captureStream || preview.mozCaptureStream
-        return new Promise(resolve => (preview.onplaying = resolve))
-      })
-      .then(() => this.start(preview.captureStream(), recordingTimeMS))
-      .then(recordedChunks => {
-        let recordedBlob = new Blob(recordedChunks, { type: 'video/webm' })
-        recording.src = URL.createObjectURL(recordedBlob)
-        console.log('src ',recording.src);
-        downloadButton.href = recording.src
-        downloadButton.download = 'RecordedVideo.webm'
-
-        this.log(
-          'Successfully recorded ' +
-            recordedBlob.size +
-            ' bytes of ' +
-            recordedBlob.type +
-            ' media.'
+    start = (stream, lengthInMS) => {
+        this.recorder = new MediaRecorder(stream, {
+          mimeType: 'video/webm; codecs=vp9'
+        })
+        // this.recorder.setAudioSource(MediaRecorder.MIC)
+        let data = []
+    
+        this.recorder.ondataavailable = event => data.push(event.data)
+        this.recorder.start()
+    
+        let stopped = new Promise((resolve, reject) => {
+          this.recorder.onstop = resolve
+          this.recorder.onerror = event => reject(event.name)
+        })
+    
+        return Promise.all([stopped]).then(() => data)
+      }
+    
+      stop = stream => {
+        stream.getTracks().forEach(track => track.stop())
+      }
+    
+      startRecording = () => {
+        navigator.mediaDevices
+          .getDisplayMedia({
+            video: {
+              cursor: 'always'
+            },
+            audio: {
+              echoCancellation: true,
+              noiseSuppression: true,
+              sampleRate: 44100
+            }
+          })
+          .then(stream => {
+            this.preview.srcObject = stream
+            this.downloadButton.href = stream
+            this.preview.captureStream =
+              this.preview.captureStream || this.preview.mozCaptureStream
+            return new Promise(resolve => (this.preview.onplaying = resolve))
+          })
+          .then(() => this.start(this.preview.captureStream()))
+          .then(recordedChunks => {
+            let recordedBlob = new Blob(recordedChunks, { type: 'video/webm' })
+            this.recording.src = URL.createObjectURL(recordedBlob)
+            console.log('src ', this.recording.src)
+            this.downloadButton.href = this.recording.src
+            this.downloadButton.download = 'RecordedVideo.webm'
+          })
+      }
+      stopRecording = () => {
+        this.recorder.state === 'recording' && this.recorder.stop()
+        this.stop(this.preview.srcObject)
+      }
+      render() {
+        return (
+          <>
+            <div className='left'>
+              <button
+                id='startButton'
+                onClick={() => this.startRecording()}
+                className='button'>
+                Start
+              </button>
+              <h2>Preview</h2>
+              <video
+                id='preview'
+                ref={videoElement => (this.preview = videoElement)}
+                width='800'
+                height='500'
+                autoPlay
+                muted
+              />
+            </div>
+            <div className='right'>
+              <button
+                id='stopButton'
+                onClick={() => this.stopRecording()}
+                className='button'>
+                Stop
+              </button>
+              <h2>Recording</h2>
+              <video
+                id='recording'
+                ref={recordingElement => (this.recording = recordingElement)}
+                width='800'
+                height='500'
+                controls
+              />
+              <button
+                id='downloadButton'
+                ref={download => (this.downloadButton = download)}
+                type='button'>
+                Download
+              </button>
+            </div>
+          </>
         )
-      })
-  }
-  stopRecording=()=>{
-    this.stop(preview.srcObject);
-  }
-  render() {
-    return (
-      <>
-        <div className='left'>
-          <button id='startButton' onClick={()=>this.startRecording()} className='button'>
-            Start
-          </button>
-          <h2>Preview</h2>
-          <video id='preview' width='800' height='500' autoPlay muted></video>
-        </div>
-
-        <div className='right'>
-          <button id='stopButton' onClick={()=>this.stopRecording()} className='button'>
-            Stop
-          </button>
-          <h2>Recording</h2>
-          <video id='recording' width='160' height='120' controls></video>
-          <button id='downloadButton' type='button'>
-            Download
-          </button>
-        </div>
-      </>
-    )
-  }
+      }
 }
 
 export default RecordingApi
