187273d3d29a1cb531cbe7966adf0e048294016f
Shakti Singh <shaktisingh230797@gmail.com>
2019-11-05 16:04:36 +0000

    Added Record RTC

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css'
 import MainPage from './components/main-page/MainPage'
 import ScribblePad from './components/scribble-pad/ScribblePad'
 // import RecordingAPI from './components/recordingApi/recordingApi2'
-import RecordingApi from './components/recordingApi/recordingApi';
-import WebCam from './components/webCam/webCam';
+// import RecordingApi from './components/recordingApi/recordingApi';
+import ScreenRecording from './components/Recording/ScreenRecording';
+import Recording from './components/Recording/Recording';
+// import WebCam from './components/webCam/webCam';
 
 function App() {
   return (
@@ -13,8 +15,8 @@ function App() {
       <Switch>
         <Route path='/' exact component={MainPage} />
         <Route path='/scribble' component={ScribblePad} />
-        <Route path='/recordingScreen' component={RecordingApi}/>
-        <Route path='/webCam' component={WebCam}/>
+        <Route path='/recordingScreen' component={ScreenRecording}/>
+        <Route path='/webCam' component={Recording}/>
         {/* <Route path="/webcamrec" component={WebCamRecorder} />
       <Route path="/audiorec" component={AudioRecorder} /> */}
       </Switch>
