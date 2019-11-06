8bc8025d6dbaa8c39fea3e22c2245c017e7c6a0b
Shivansh Tyagi <stshivansh9@gmail.com>
2019-11-06 21:38:38 +0530

    app js edit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import MainPage from './components/main-page/MainPage'
 import ScribblePad from './components/scribble-pad/ScribblePad'
 import createLecture from './components/createLecture/CreateLecture'
 import ReactPath from './lib/ReactPath'
+import { Route, Switch } from 'react-router'
 
 function App() {
    return (
       <div className='App'>
          <Switch>
-            <Route path={ReactPath.homePath} exact component={MainPage}/>
-            <Route path={ReactPath.scribble} component={ScribblePad}/>
-            <Route path={ReactPath.createLecture} component={createLecture}/>
+            <Route path={ReactPath.homePath} exact component={MainPage} />
+            <Route path={ReactPath.scribble} component={ScribblePad} />
+            <Route path={ReactPath.createLecture} component={createLecture} />
             {/*<Route path="/webcamrec" component={WebCamRecorder} />*/}
             {/*<Route path="/audiorec" component={AudioRecorder} />*/}
          </Switch>
