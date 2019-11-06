8bd1405f50c5617ed5711dde2bc73ca506d55402
nik72619c <nikhilsharmarockstar21@gmail.com>
2019-11-06 17:35:02 +0530

    added comments section for a video

---
+++ src/components/comments/Comments.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import CommentsBlock from 'simple-react-comments'
+
+export default class Comments extends React.Component {
+   constructor(props) {
+      super(props)
+      this.state = {
+         comments: [
+            {
+               authorUrl: '#',
+               avatarUrl:
+                  'https://image.shutterstock.com/image-vector/user-icon-trendy-flat-style-260nw-418179865.jpg',
+               createdAt: new Date(),
+               fullName: 'Name',
+               text: 'some dummy comment !'
+            }
+         ]
+      }
+   }
+   render() {
+      return (
+         <div>
+            <CommentsBlock
+               comments={this.state.comments}
+               signinUrl={'/signin'}
+               isLoggedIn
+               reactRouter={false} // set to true if you are using react-router
+               onSubmit={text => {
+                  if (text.length > 0) {
+                     this.setState({
+                        comments: [
+                           ...this.state.comments,
+                           {
+                              authorUrl: '#',
+                              avatarUrl:
+                                 'https://image.shutterstock.com/image-vector/user-icon-trendy-flat-style-260nw-418179865.jpg',
+                              createdAt: new Date(),
+                              fullName: 'Name',
+                              text
+                           }
+                        ]
+                     })
+                     console.log('submit:', text)
+                  }
+               }}
+            />
+         </div>
+      )
+   }
+}
