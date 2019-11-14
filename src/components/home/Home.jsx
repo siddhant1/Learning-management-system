571c2d996f3e5e586ad2fb39d736919d3c28dfcd
siddhant <30566406+siddhant1@users.noreply.github.com>
2019-11-14 23:11:41 +0530

    added mehngi vali animation (#38)

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
+import { fadeIn } from 'react-animations'
+import { ArrowRight } from 'react-feather'
+import { PortalWithState } from 'react-portal'
+import { Button, Input, InputGroup, InputGroupAddon } from 'reactstrap'
+import styled, { keyframes } from 'styled-components'
+import CreatePlaylistActionButton from '../create-playlist/CreatePlaylistActionButton'
 import Videos from '../videos/Videos'
 import './home.css'
-import CreatePlaylistActionButton from '../create-playlist/CreatePlaylistActionButton'
+
+const bounceAnimation = keyframes`${fadeIn}`
+
+const FadeInDiv = styled.div`
+   display: flex;
+   animation: 3s ${bounceAnimation};
+`
 
 export default class Home extends Component {
+   state = {
+      showOptions: false,
+      images: [
+         { name: 'Artboard – 1.png', route: '/webCam' },
+         { name: 'Artboard – 2.png', route: '/pen' },
+         { name: 'Artboard – 3.png', route: '/screenRecord' },
+         { name: 'Artboard – 4.png', route: '/text' },
+         { name: 'Artboard – 5.png', route: '/uploadImage' }
+      ]
+   }
    render() {
       return (
          <div className='home-page-content'>
@@ -11,9 +33,65 @@ export default class Home extends Component {
                Recently Created Videos
             </div>
             <Videos />
-            <CreatePlaylistActionButton
-               callback={() => this.props.history.push('/newPlaylist')}
-            />
+            <PortalWithState closeOnEsc>
+               {({ openPortal, closePortal, isOpen, portal }) => (
+                  <React.Fragment>
+                     <CreatePlaylistActionButton
+                        callback={isOpen ? closePortal : openPortal}
+                     />
+                     {portal(
+                        <div className='portal'>
+                           <div style={{ display: 'flex', marginTop: '20px' }}>
+                              <p style={{ marginTop: '7px' }}>
+                                 Enter Playlist Name:
+                              </p>
+
+                              <InputGroup>
+                                 <Input />
+                                 <InputGroupAddon addonType='append'>
+                                    <Button
+                                       onClick={() =>
+                                          this.setState({ showOptions: true })
+                                       }>
+                                       <ArrowRight />
+                                    </Button>
+                                 </InputGroupAddon>
+                              </InputGroup>
+                           </div>
+                           <br />
+                           {this.state.showOptions && (
+                              <div style={{ display: 'block' }}>
+                                 <div>
+                                    {this.state.images.map(img => (
+                                       <div
+                                          className='method'
+                                          style={{
+                                             display: 'inline-block',
+                                             marginTop: 0
+                                          }}>
+                                          <FadeInDiv>
+                                             <img
+                                                className='pl-img'
+                                                key={img.name}
+                                                src={`./images/${img.name}`}
+                                                alt={img.name}
+                                                onClick={() =>
+                                                   this.props.history.push(
+                                                      img.route
+                                                   )
+                                                }
+                                             />
+                                          </FadeInDiv>
+                                       </div>
+                                    ))}
+                                 </div>
+                              </div>
+                           )}
+                        </div>
+                     )}
+                  </React.Fragment>
+               )}
+            </PortalWithState>
          </div>
       )
    }
--- src/components/main-page/MainPage.jsx
+++ src/components/main-page/MainPage.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import Header from '../header/header'
-import AddLecture from '../addLecture/AddLecture'
-import ScribblePad from '../scribble-pad/ScribblePad'
 import ReactPath from '../../lib/ReactPath'
-import Home from '../home/Home'
+import AddLecture from '../addLecture/AddLecture'
 import CreateNewPlaylist from '../create-new-playlist/CreateNewPlaylist'
+import Header from '../header/header'
+import Home from '../home/Home'
+import ScribblePad from '../scribble-pad/ScribblePad'
+import Forgetpassword from '../UserLogin/forgetpassword'
 import Login from '../UserLogin/login'
 import SignUp from '../UserLogin/signup'
 import VideoPlayer from '../VideoPlayer'
-import Forgetpassword from '../UserLogin/forgetpassword'
-import Recording from '../Recording/Recording'
 import ScreenRecording from '../Recording/ScreenRecording'
+import Recording from '../Recording/Recording'
 
 export default class MainPage extends React.Component {
    render() {
@@ -22,6 +22,13 @@ export default class MainPage extends React.Component {
                <Route exact path={ReactPath.homePath} component={Home} />
                <Route exact path={ReactPath.login} component={Login} />
                <Route exact path={ReactPath.signup} component={SignUp} />
+               <Route
+                  exact
+                  path={ReactPath.forgetpassword}
+                  component={Forgetpassword}
+               />
+               <Route exact path={ReactPath.scribble} component={ScribblePad} />
+               <Route exact path={ReactPath.createLesson} />
                <Route
                   exact
                   path={ReactPath.forgetpassword}
@@ -43,6 +50,8 @@ export default class MainPage extends React.Component {
                   path={ReactPath.playVideo}
                   component={VideoPlayer}
                />
+               {/* <Route path={ReactPath.webCam} component={Recording}/>
+               <Route path={ReactPath.screenRecord} component={ScreenRecording}/> */}
                <Route
                   exact
                   path={ReactPath.screenRecord}
